890bc8cc906b96039d5a802d2488a64af74a6e07
Eugenia <eugeniamata1@hotmail.com>
2021-06-29 15:51:37 +0200

    29-6

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const API_KEY = '54f1c127920de15bda65f23b007b9ea3'; 
-const URL = 'http://www.theaudiodb.com/api/v1/json/';
+const URL = 'https://www.theaudiodb.com/api_guide.php';
 
 const button = document.getElementById("sendButton");
 const main = document.getElementById("main");
@@ -9,64 +9,61 @@ const h3 = document.getElementById("h3");
 
 
 button.addEventListener("click", ()=>{
-  searchWeather(inputElement.value);
+  searchArtist(inputElement.value);
 });
 
+//theaudiodb.com/api/v1/json/{APIKEY}/track-top10.php?s=(artistname}
+//theaudiodb.com/api/v1/json/{APIKEY}/artist.php?i=112024
+//theaudiodb.com/api/v1/json/1/search.php?s=${wordToSearch}
+//theaudiodb.com/api/v1/json/1/discography.php?s=${wordToSearch}`
 
-function searchWeather(wordToSearch){
+
+function searchArtist(wordToSearch){
   console.log('Palabra', wordToSearch);
+ 
+ // theaudiodb.com/api/v1/json/{APIKEY}/discography.php?s=coldplay
 
-  const fetchPromise = fetch(`https://www.theaudiodb.com/api/v1/json/{API_KEY}/discography.php?s=coldplay`);
+  const fetchPromise = fetch(`https://theaudiodb.com/api/v1/json/1/search.php?s=${wordToSearch}`);
+
+  
  
   fetchPromise.then(response => {
   
     return response.json();
   }).then(result => {
     console.log('data', result);
-    weather = result.list[0].weather[0].main;
-    console.log(weather);
+     artist = result.artists[0].strArtist;
+    console.log(artist);
     madeGrid(result);
-    cambiarImagenJS(weather,wordToSearch);
   }).catch(err =>{
     console.log('salio mal!: ', err);
   });
 
 
-function cambiarImagenJS(weather,wordToSearch){
-  console.log('funcion' , weather);
-  console.log('funcion' , wordToSearch);
-  if (weather == "Rain") {
-    img.src = "img/clima.png";
-   h3.innerHTML = `${wordToSearch} -  Lluvia`;
-  
-  } else if (weather == "Clear") {
-    img.src = "img/dom.png";
-   h3.innerHTML = `${wordToSearch} -  Despejado`;
-
-   } else if (weather == "Clouds") {
-    img.src = "img/nube.png";
-    h3.innerHTML = `${wordToSearch} -  Nublado`;
-
-   }  else if (weather == "Snow") {
-    img.src = "img/snowflake.png";
-    h3.innerHTML = `${wordToSearch} -  Nieve`;
-
-   } 
-}
-
 
 }
 
 function madeGrid(data){
-  const temp = data.list[0].main.temp ;
-  const temp_max = data.list[0].main.temp_max ;
-  const temp_min = data.list[0].main.temp_min ;
-  const humidity = data.list[0].main.humidity ;
-  const feels = data.list[0].main.feels_like ;
-  const pressure = data.list[0].main.pressure ;
-  const wind = data.list[0].wind.speed ;
+  const artist = data.artists[0].strArtist ;
+  
+  const bio = data.artists[0].strBiographyES ;
+const genero = data.artists[0].strGenre ;
+const fb = data.artists[0].strFacebook ;
+const tw = data.artists[0].strTwitter ;
+const web = data.artists[0].strWebsite ;
 
-  main.innerHTML = `<ul><li><p>Temperatura: ${temp} ºC</p></li><li><p>Temp min: ${temp_min} ºC</p></li><li><p>Temp max: ${temp_max} ºC</p></li><li><p>Humedad: ${humidity} %</p></li><li><p>Sensación Térmica: ${feels} ºC</p></li><li><p>Presión Atmosférica: ${pressure} hPa</p></li><li><p>Velocidad del viento: ${wind} meter/sec</p></li></ul>`
+const banner = data.artists[0].strArtistFanart ;
+
+  main.innerHTML = `<ul><li><p>Artista: ${artist} </p></li>
+
+                        <li><p>Género: ${genero} </p></li>
+                        <li><p>Biografía: ${bio} </p></li>
+                        <li><p>Facebook: ${fb} </p></li>
+                        <li><p>Twitter: ${tw} </p></li>
+                        <li><p>Web: ${web} </p></li>
+                        <img class="d-block w-100" src="${banner}" alt="Imagen del Artista en cuestión">
+ 
+  </ul>`
 
 
 }
